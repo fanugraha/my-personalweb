5c654781d895490628978eea63ec5c60a9fbdedd
fanugraha <nugrahafajarari.com>
2023-09-14 17:28:44 +0700

    Perbaiki src

--- src/componen/Navbar.jsx
+++ src/componen/Navbar.jsx
@@ -4,13 +4,13 @@ const Navbar = () => {
       <div className="logo">Fajar Ari Nugraha</div>
       <ul>
         <li>
-          <a href="">Home</a>
+          <a href="#">Home</a>
         </li>
         <li>
-          <a href="">Download CV</a>
+          <a href="#">Download CV</a>
         </li>
         <li>
-          <a href="">Contact</a>
+          <a href="#">Contact</a>
         </li>
       </ul>
     </nav>
